a43bf9e67a23be0c3e425a241bdd9f86f83d2b76
Rumet Şaşmaz <rumetarges@gmail.com>
2026-08-21 23:12:32 +0300

    Delete siber directory

--- siber/threat_detect.py
+++
@@ -1,9 +0,0 @@
-def detect_threat(ip_list): 
-    for ip in ip_list:
-     if ip == "192.168.1.100":
-        print(f"Threat Found {ip}")
-
-
-ips = ["10.0.0.1", "192.168.1.100", "172.16.0.1"]
-
-detect_threat(ips)
