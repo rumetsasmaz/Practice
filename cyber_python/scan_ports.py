4be6a5a3d23696d144f097adde9a0c6ae52824e1
rumetsasmaz <rumetarges@gmail.com>
2026-09-15 04:00:36 +0300

    scan open ports

--- cyber_python/scan_ports.py
+++ cyber_python/scan_ports.py
@@ -1,26 +1,29 @@
 import socket
-def scan_ports(port_list):
 
+def scan_port_range(start_port , end_port):
     target_ip = "127.0.0.1"
+   
+    print(f"[+] Scanning {target_ip} from port {start_port} to {end_port}...")
 
-    print(f"[+] Started port scan at...\n{target_ip} ")
-
-    for port in port_list: # type: ignore
+    for port in range(start_port , end_port + 1 ):
         s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
         s.settimeout(0.5)
 
-        result = s.connect_ex((target_ip, port))
+        result = s.connect_ex((target_ip , port))
 
         if result == 0:
             print(f"[SUCCESS] Port {port}: OPEN")
         else:
-            print(f"[FAILED] Port {port}: CLOSED")
-
+            print(f"[FAILED] Port {port} CLOSED") 
 
         s.close()
 
-ports = [21, 22, 80, 135, 443, 445]
 
-scan_ports(ports)
 
-print(f"\n[+] Scan Completed")
+
+try:
+    start = int(input("Enter A Start Port: "))
+    end = int(input("Enter A End Port: "))
+    scan_port_range(start , end)       
+except ValueError:
+    print("[-] Error: Please enter a valid port number!")
